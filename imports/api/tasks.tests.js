65354c654778406a134ecc097d654aaf81d72233
Vivi-Iam <vivian.opondoh@gmail.com>
2019-01-08 17:43:41 +0300

    Create test file and add can delete owned task test

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -1,19 +1,24 @@
+//makes mocha know test file
 /* eslint-env mocha */
  
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
-import { assert } from 'meteor/practicalmeteor:chai';
+import { assert } from 'chai';
+// import { Accounts } from 'meteor/accounts-base';
 
 import { Tasks } from './tasks.js';
 
+//checks is running on the server side
 if (Meteor.isServer) {
  describe('Tasks', () => {
    describe('methods', () => {
     const userId = Random.id();
     let taskId;
-
+//befor every test
     beforeEach(() => {
+      //remove all tasks
       Tasks.remove({});
+      //create task and equate taskId
       taskId = Tasks.insert({
         text: 'test task',
         createdAt: new Date(),
@@ -21,6 +26,7 @@ if (Meteor.isServer) {
         username: 'tmeasday',
       });
     });
+    //write test shows that you can delete your own task
      it('can delete owned task', () => {
          // Find the internal implementation of the task method so we can
         // test it in isolation
