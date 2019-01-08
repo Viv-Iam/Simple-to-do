21f048d6be291d8ec1df9d6ba69b693c0aaae6c9
Vivi-Iam <vivian.opondoh@gmail.com>
2019-01-08 18:05:44 +0300

    Add comments to code

--- tests/main.js
+++ tests/main.js
@@ -3,7 +3,9 @@ import "../imports/api/tasks.tests.js"
 
 describe("simple-todos", function () {
   it("package.json has correct name", async function () {
+    //{name} is name attribute of the file in package.json file
     const { name } = await import("../package.json");
+    //ensure name === simple-todos
     assert.strictEqual(name, "simple-todos");
   });
 
