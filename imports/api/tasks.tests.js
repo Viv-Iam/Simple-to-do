6836c5358a128c5cc5644f383ef4c979cc45b1c9
Vivi-Iam <vivian.opondoh@gmail.com>
2019-01-10 14:08:06 +0300

    Add comments

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -20,13 +20,7 @@ if (Meteor.isServer) {
     describe('methods', () => {
       const userId = Random.id();
       let taskId;
-      // const user = 'vivian';
-      // let text;
-      // Meteor.users.find
-      // befor(function()) {
-      // userId = user._id
-      // }
-      //befor every test
+
       beforeEach(() => {
         //remove all tasks
         Tasks.remove({});
@@ -38,6 +32,8 @@ if (Meteor.isServer) {
           username: 'tmeasday',
         });
       });
+
+      // *******************DELETE****************
 
       //write test shows that you can delete your own task
       it('can delete owned task', () => {
@@ -57,8 +53,6 @@ if (Meteor.isServer) {
 
       //write test shows that you cannot delete others task
       it('cannot delete others task', () => {
-        let setToPrivate = true
-        
 
         //set existing task private by updating the task
         Tasks.update(taskId, { $set: { private: true } });
@@ -85,6 +79,8 @@ if (Meteor.isServer) {
         assert.equal(Tasks.find().count(), 1);
       });
 
+      // *******************INSERT****************
+
       //write test shows that you can insert task
       it('can insert owned tasks', () => {
         let text = "text"
@@ -93,9 +89,7 @@ if (Meteor.isServer) {
         const insertTask = Meteor.server.method_handlers['tasks.insert'];
 
         // Set up a fake method invocation that looks like what the method expects
-        const invocation = {
-          userId
-        };
+        const invocation = { userId };
 
         // Run the method with `this` set to the fake invocation
         insertTask.apply(invocation, [text]);
@@ -104,48 +98,42 @@ if (Meteor.isServer) {
         assert.equal(Tasks.find().count(), 2);
       });
 
+       // *******************SETCHECKED****************
+
       //write test shows that you can setChecked task
       it('can setChecked task', () => {
-        let setChecked = true
 
         // Find the internal implementation of the task method so we can
         // test it in isolation
         const setCheckedTask = Meteor.server.method_handlers['tasks.setChecked'];
 
         // Set up a fake method invocation that looks like what the method expects
-        const invocation = {
-          userId
-        };
+        const invocation = { userId };
 
         // Run the method with `this` set to the fake invocation
-        setCheckedTask.apply(invocation, [taskId, setChecked]);
+        setCheckedTask.apply(invocation, [taskId, true]);
 
         // Verify that the method does what we expected
-        assert.equal(Tasks.find(taskId, { $set: { checked: setChecked } }).count(), 1);
+        assert.equal(Tasks.find(taskId, { $set: { checked: true } }).count(), 1);
       });
+
+       // *******************SETTOPRIVATE****************
 
       //write test shows that you can setToPrivate task
       it('can setToPrivate task', () => {
-        let setToPrivate = true
 
         // Find the internal implementation of the task method so we can
         // test it in isolation
         const setToPrivateTask = Meteor.server.method_handlers['tasks.setPrivate'];
 
         // Set up a fake method invocation that looks like what the method expects
-        const invocation = {
-          userId
-        };
+        const invocation = { userId };
 
         // Run the method with `this` set to the fake invocation
-        setToPrivateTask.apply(invocation, [taskId, setToPrivate]);
+        setToPrivateTask.apply(invocation, [taskId, true]);
 
         // Verify that the method does what we expected
-        assert.equal(Tasks.find(taskId, {
-          $set: {
-            private: setToPrivate
-          }
-        }).count(), 1);
+        assert.equal(Tasks.find(taskId, { $set: { private: true } }).count(), 1);
       });
 
     });
