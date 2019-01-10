f8341d912573dcfeeaf2e1e13a7e82711f30ea18
Vivi-Iam <vivian.opondoh@gmail.com>
2019-01-10 18:09:52 +0300

    Add test for can delete someones public task

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -65,7 +65,7 @@ if (Meteor.isServer) {
       });
 
       //write test shows that you cannot delete others task
-      it('cannot delete others task', () => {
+      it('cannot delete someones task', () => {
 
         //set existing task private by updating the task
         Tasks.update(taskId, { $set: { private: true } });
@@ -90,6 +90,24 @@ if (Meteor.isServer) {
 
         // Verify that task is not deleted
         assert.equal(Tasks.find().count(), 1);
+      });
+
+      //write test shows that you can delete someones public task
+      it('can delete someones public task', () => {
+        const anotheruserId = Random.id();
+
+        // Find the internal implementation of the task method so we can
+        // test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+
+        // Set up a fake method invocation that looks like what the method expects
+        const invocation = { 'userId': anotheruserId };
+
+        // Run the method with `this` set to the fake invocation
+        deleteTask.apply(invocation, [taskId]);
+
+        // Verify that the method does what we expected
+        assert.equal(Tasks.find().count(), 0);
       });
 
       // *******************INSERT****************
